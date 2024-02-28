86ba5dd99327e9c3222a12a866148c704c2287e5
jean519 <129189905+jean519@users.noreply.github.com>
2024-02-28 14:37:56 +0100

    pin

--- add.js
+++ add.js
@@ -1,19 +1,41 @@
-var Person = /** @class */ (function () {
-    function Person(name, age) {
-        this.name = name;
-        this.age = age;
+"use strict";
+// class Person {
+//   private name: string;
+//   private age: number;
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   tellMyName() {
+//     console.log(`I am ${this.name}`);
+//   }
+//   tellMyAge() {
+//     console.log(`I am ${this.age} years old`);
+//   }
+// }
+// const test1 = new Person("Jean-terre", 25);
+// test1.tellMyName();
+// test1.tellMyAge();
+// const test2 = new Person("Marie-madeleine", 156);
+// test2.tellMyName();
+// test2.tellMyAge();
+class bankCustomer {
+  constructor(name, pin) {
+    this.name = name;
+    this.pin = pin;
+  }
+  getName() {
+    return this.name;
+  }
+  verifyPinInput() {
+    if (this.pin === 1234) {
+      return true;
+    } else {
+      return false;
     }
-    Person.prototype.tellMyName = function () {
-        console.log("I am ".concat(this.name));
-    };
-    Person.prototype.tellMyAge = function () {
-        console.log("I am ".concat(this.age, " years old"));
-    };
-    return Person;
-}());
-var test1 = new Person("Jean-terre", 25);
-test1.tellMyName();
-test1.tellMyAge();
-var test2 = new Person("Marie-madeleine", 156);
-test2.tellMyName();
-test2.tellMyAge();
+  }
+}
+const customer = new bankCustomer("John Doe", 1234);
+const customer2 = new bankCustomer("Jane Doe", 5678);
+console.log(customer.verifyPinInput());
+console.log(customer2.verifyPinInput());
--- add.ts
+++ add.ts
@@ -1,25 +1,53 @@
-class Person {
+// class Person {
+//   private name: string;
+//   private age: number;
+
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
+
+//   tellMyName() {
+//     console.log(`I am ${this.name}`);
+//   }
+
+//   tellMyAge() {
+//     console.log(`I am ${this.age} years old`);
+//   }
+// }
+
+// const test1 = new Person("Jean-terre", 25);
+// test1.tellMyName();
+// test1.tellMyAge();
+
+// const test2 = new Person("Marie-madeleine", 156);
+// test2.tellMyName();
+// test2.tellMyAge();
+
+class bankCustomer {
   private name: string;
-  private age: number;
+  private pin: number;
 
-  constructor(name: string, age: number) {
+  constructor(name: string, pin: number) {
     this.name = name;
-    this.age = age;
+    this.pin = pin;
   }
 
-  tellMyName() {
-    console.log(`I am ${this.name}`);
+  getName(): string {
+    return this.name;
   }
 
-  tellMyAge() {
-    console.log(`I am ${this.age} years old`);
+  verifyPinInput(): boolean {
+    if (this.pin === 1234) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
-const test1 = new Person("Jean-terre", 25);
-test1.tellMyName();
-test1.tellMyAge();
+const customer = new bankCustomer("John Doe", 1234);
+const customer2 = new bankCustomer("Jane Doe", 5678);
 
-const test2 = new Person("Marie-madeleine", 156);
-test2.tellMyName();
-test2.tellMyAge();
+console.log(customer.verifyPinInput());
+console.log(customer2.verifyPinInput());
